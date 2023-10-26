a4f6589a035aa4c8d5a8be48c81c59f4aa334afd
Emi <emiliano.lopez@ing.austral.edu.ar>
2023-10-26 00:14:33 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import Tetris from "react-tetris";
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => (
+  <div>
+    <h1>Tetris</h1>
+    <Tetris
+      keyboardControls={{
+        // Default values shown here. These will be used if no
+        // `keyboardControls` prop is provided.
+        down: 'MOVE_DOWN',
+        left: 'MOVE_LEFT',
+        right: 'MOVE_RIGHT',
+        space: 'HARD_DROP',
+        z: 'FLIP_COUNTERCLOCKWISE',
+        x: 'FLIP_CLOCKWISE',
+        up: 'FLIP_CLOCKWISE',
+        p: 'TOGGLE_PAUSE',
+        c: 'HOLD',
+        shift: 'HOLD'
+      }}
+    >
+      {({
+        HeldPiece,
+        Gameboard,
+        PieceQueue,
+        points,
+        linesCleared,
+        state,
+        controller
+      }) => (
+        <div>
+          {
+            //<HeldPiece />
+          }
+          <div>
+            <p>Points: {points}</p>
+            <p>Lines Cleared: {linesCleared}</p>
+          </div>
+          <Gameboard />
+          {
+          //<div className="piece-queue">
+          //<PieceQueue/>
+          //</div>
+          }
+          
+          {state === 'LOST' && (
+            <div>
+              <h2>Game Over</h2>
+              <button onClick={controller.restart}>New game</button>
+            </div>
+          )}
+        </div>
+      )}
+    </Tetris>
+  </div>
+);
 
 export default App;
